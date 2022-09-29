1865bd1faeb464983a7cc2dcef90b4ed40656148
Shaurya Gupta <shauryag1000@gmail.com>
2022-09-29 21:19:38 +0530

    added light and dark mode

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 // add you code here
-
-
-// ---------------------------------- Ui-3 Js code -------------------------------------
-// ---------------------------------- Ui-4 Js code -------------------------------------
+function toggledarklight(){
+    var element=document.body;
+    var mode=document.getElementById("mode");
+      if(element.className=="light-mode"){
+          element.className="dark-mode";
+        mode.innerText="Light";
+      }
+      else{
+      element.className="light-mode";
+      mode.innerText="Dark";
+      }
+    }
+    
+    // ---------------------------------- Ui-3 Js code -------------------------------------
+    // ---------------------------------- Ui-4 Js code -------------------------------------
