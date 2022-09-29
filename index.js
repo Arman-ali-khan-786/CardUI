89c0e57431ab612604f376e5f103db9d38071b88
Saikiran voladri <200303124537@paruluniversity.ac.in>
2022-09-29 12:23:17 +0530

    added code from html to javascript file

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 
 // ---------------------------------- Ui-3 Js code -------------------------------------
 // ---------------------------------- Ui-4 Js code -------------------------------------
+
+
+// ---------------------------------- Ui-7 Js code starts -------------------------------------
+const img = document.querySelector("img");
+const icons = document.querySelector(".icons");
+img.onclick = function(){
+  this.classList.toggle("active");
+  icons.classList.toggle("active");}
+  // ---------------------------------- Ui-7 Js code  ends-------------------------------------
