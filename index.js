08735025abbd2a5648f4f343d28b7a8574699bdf
Akshyat Patra <akshyatpatra@gmail.com>
2023-05-06 18:56:01 +0530

    Added Card UI 22 - Akshyat Patra

--- index.js
+++ index.js
@@ -37,3 +37,23 @@ darkModeBtn.addEventListener('click', () => {
               this.classList.toggle("active");
               icons.classList.toggle("active");}
 
+// ---------------------------------- Ui-22 Code Begins Here --------------------------------
+
+const showSocial = (toggleCard, socialCard) => {
+    const toggle = document.getElementById(toggleCard),
+      social = document.getElementById(socialCard);
+  
+    toggle.addEventListener("click", () => {
+      if (social.classList.contains("animation")) {
+        social.classList.add("down-animation");
+  
+        setTimeout(() => {
+          social.classList.remove("down-animation");
+        }, 1000);
+      }
+      social.classList.toggle("animation");
+    });
+  };
+  
+  showSocial("card-toggle", "card-social");
+  
